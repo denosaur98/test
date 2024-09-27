dfc6437e56631c75c127c256aed2736dfd915533
denosaur98 <denischurchill1998@gmail.com>
2024-09-27 14:23:03 +0300

    добавил редирект для главной

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import SpaceshipList from '@/pages/SpaceshipList.vue'
 import SpaceshipPage from '@/pages/SpaceshipPage.vue'
 
 const routes = [
+	{ path: '/', redirect: '/starships/page/1' },
 	{ path: '/starships/page/:page', component: SpaceshipList },
 	{ path: '/starships/:id', component: SpaceshipPage },
 ]
